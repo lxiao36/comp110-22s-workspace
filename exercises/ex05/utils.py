e1f99a3f8cf7c4c6e046265ef82c1f4af2d748ab
Lei Xiao <97568440+lxiao36@users.noreply.github.com>
2022-02-24 11:09:15 -0500

    some adjustment to ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,9 +1,10 @@
-"""Some 'list' Utility Functions."""
+"""Some list Utility Functions."""
 
 __author__ = "730526509"
 
 
 def only_evens(nums: list[int]) -> list[int]:
+    """Insert list then will return even numbers."""
     x: list[int] = list()
     for num in nums:
         if num % 2 == 0:
@@ -12,7 +13,15 @@ def only_evens(nums: list[int]) -> list[int]:
 
 
 def sub(a_lists: list[int], start: int, end: int) -> list[int]:
+    """Returns numbers between start and end -1."""
     x: list[int] = list()
+    max_length = len(a_lists)
+    if start < 0:
+        start = 0
+    if end > max_length:
+        end = max_length
+    if max_length == 0 or start > max_length or end <= 0:
+        a_lists = list()
     while start != end:
         x.append(a_lists[start])
         start += 1
@@ -20,6 +29,7 @@ def sub(a_lists: list[int], start: int, end: int) -> list[int]:
 
 
 def concat(first: list[int], second: list[int]) -> list[int]:
+    """Returns two lists put together."""
     dex = len(second)
     i = 0
     while i != dex:
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -7,50 +7,59 @@ from utils import only_evens, sub, concat
 
 # test only_even
 def test_only_evens_edge() -> None:
+    """A edge case test for only_even."""
     nums: list[int] = []
     assert only_evens(nums) == []
 
 
 def test_only_evens_use() -> None:
+    """A use case test for only_even."""
     nums: list[int] = [1, 2, 3]
     assert only_evens(nums) == [2]
 
 
 def test_only_evens_use_two() -> None:
-    nums: list[int] = [1, 2, 3]
-    assert only_evens(nums) == [2]
+    """A second use case test for only_even."""
+    nums: list[int] = [1, 2, 3, 4]
+    assert only_evens(nums) == [2, 4]
 
 
 # test sub
 def test_sub_edge_() -> None:
-    a_list: list[int] = [40, 40, 40, 40]
-    assert sub(a_list, 1, 4) == [40, 40, 40]
+    """A edge case test for sub."""
+    a_list: list[int] = []
+    assert sub(a_list, 0, 0) == []
 
 
 def test_sub_use() -> None:
-    a_list: list[int] = [10, 20, 30, 40]
-    assert sub(a_list, 1, 3) == [20, 30]
+    """A use case test for sub."""
+    a_list: list[int] = [-1, 0, 1, 2]
+    assert sub(a_list, 1, 5) == [0, 1, 2]
 
 
 def test_sub_use_two() -> None:
+    """A second use case test for sub."""
     a_list: list[int] = [1, 2, 3, 4, 5, 6]
     assert sub(a_list, 1, 5) == [2, 3, 4, 5]
 
 
 # test concat
 def test_concat_edge() -> None:
+    """A edge case test for concat."""
     first: list[int] = [0]
     second: list[int] = [0]
     assert concat(first, second) == [0, 0]
 
 
 def test_concat_use() -> None:
+    """A use case test for concat."""
     first: list[int] = [11, 22, 22, 11]
     second: list[int] = [22, 11, 11, 22]
     assert concat(first, second) == [11, 22, 22, 11, 22, 11, 11, 22]
 
 
 def test_concat_use_two() -> None:
+    """A second use case test for concat."""
     first: list[int] = [1, 2, 3, 4]
     second: list[int] = [1, 2, 3, 4]
     assert concat(first, second) == [1, 2, 3, 4, 1, 2, 3, 4]
